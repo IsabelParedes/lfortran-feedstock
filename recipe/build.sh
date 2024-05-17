19ef2fcd56d72c5bae3cc3ae8b78c732081aa2ea
Isabel Paredes <isabel.paredes@quantstack.net>
2024-05-17 14:26:47 +0200

    Modify recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,56 +4,43 @@ set -ex
 
 export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS -D_LIBCPP_DISABLE_AVAILABILITY"
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  WRT=no
-  (
-    export CC=${CC_FOR_BUILD}
-    export CXX=${CXX_FOR_BUILD}
-    export NM=$($CC_FOR_BUILD -print-prog-name=nm)
-    export AR=$($CC_FOR_BUILD -print-prog-name=ar)
-    export OBJCOPY=$($CC_FOR_BUILD -print-prog-name=objcopy)
-    export STRIP=$($CC_FOR_BUILD -print-prog-name=strip)
 
-    # CFLAGS and CXXFLAGS are intentionally empty
-    unset CFLAGS
-    unset CXXFLAGS
-    unset CPPFLAGS
+WRT=yes
 
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-
-    mkdir build-native
-    cd build-native
-    cmake ${CMAKE_ARGS} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
-        -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX \
-        -DCMAKE_PREFIX_PATH=$BUILD_PREFIX \
-        -DWITH_LLVM=yes \
-        -DWITH_XEUS=yes \
-        -DWITH_RUNTIME_LIBRARY=yes \
-        -DWITH_RUNTIME_STACKTRACE=yes \
-        -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
-        $SRC_DIR
-    make
-  )
-else
-  WRT=yes
-fi
 
 mkdir build
 cd build
 export LFORTRAN_CC=${CC}
-cmake ${CMAKE_ARGS} \
+export EMSDK_PATH=${CONDA_EMSDK_DIR}
+# export WASI_SDK_PATH=""
+
+chmod 777 ${SRC_DIR}/ci/version.sh
+
+cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
+    -DWITH_LLVM=yes \
+    -DLFORTRAN_BUILD_ALL=yes \
+    -DWITH_STACKTRACE=yes \
+    -DWITH_RUNTIME_STACKTRACE=no \
+    -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DWITH_LLVM=yes \
-    -DWITH_XEUS=yes \
-    -DWITH_RUNTIME_LIBRARY=$WRT \
-    -DWITH_RUNTIME_STACKTRACE=yes \
     -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
+    -DWITH_TARGET_WASM=yes \
     $SRC_DIR
+
+# cmake ${CMAKE_ARGS} \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_CXX_FLAGS_RELEASE="-Wall -Wextra -O3 -funroll-loops -DNDEBUG" \
+#     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#     -DCMAKE_PREFIX_PATH=$PREFIX \
+#     -DWITH_LLVM=yes \
+#     -DWITH_XEUS=no \
+#     -DWITH_TARGET_WASM=yes \
+#     -DWITH_RUNTIME_LIBRARY=no \
+#     -DWITH_RUNTIME_STACKTRACE=no \
+#     -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
+#     $SRC_DIR
 
 make -j${CPU_COUNT}
 make install
